400a228eba37d9f6284a5b6d541a4f114b4cf65c
Arief Syahroni <riefsyah31@gmail.com>
2021-07-31 14:55:50 +0700

    setting up project and add some assets

---
+++ lib/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final double defaultMargin = 24.0;
+
+Color blueColor = Color(0XFF5589F0);
+Color lightBlueColor = Color(0XFF4E82EA);
+Color semiLightGreyColor = Color(0XFFD8D8D8);
+Color greyColor = Color(0XFFB7B7B7);
+Color yellowColor = Color(0XFFFFB25A);
+Color redColor = Color(0XFFF2735B);
+Color lightGreyColor = Color(0XFFFBFBFB);
+Color whiteColor = Color(0XFFFFFFFF);
+Color blackColor = Color(0XFF2A2B3D);
+
+TextStyle regularTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle mediumTextstyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle semiBoldTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w600,
+);
